5435325025d38c9d1246d6350cb0d4a2eee87c7e
hsk <sakurai777@gmail.com>
2018-03-05 02:07:56 +0900

    recent changes

--- scala/mss/src/main/scala/mss2.scala
+++ scala/mss/src/main/scala/mss2.scala
@@ -30,7 +30,6 @@ object mss2 {
       | record(l=e) | e#l | modify(e,l,e)
       | {[l=e]} | case(e,variant(l=e)).
   */
-
   trait E
   case object ETrue extends E
   case object EFalse extends E
@@ -93,16 +92,6 @@ object mss2 {
         case (l,e)::ls => find((l,e)::hs,ls)
       }
       find(List(),les)
-      /*
-      les.find{(l,e)=>!v(e)} match {
-        Some((l1,e1)) =>
-          evHole(e1) match {
-            (e,h) => (e,{e=>ERecord(les.map{
-              (l,e1)=> if (l==l1) (l,e) else (l,e1)
-            })})
-          }
-        None => (ERecord(les),{e=>e})
-      }*/
     case EDot(e,l) =>
       evHole(e) match{case(e,h)=>(e,{e=>EDot(h(e),l)})}
     case EModify(e1,l,e2) if !v(e1) =>
@@ -236,13 +225,19 @@ object mss2 {
 
   eftv(K,σ,EFTV) :- ftv(σ,FTV),member(σ:k,K),kftv(k,FTV2),union(FTV,FTV2,EFTV).
   eftv(_,σ,FTV) :- ftv(σ,FTV).
+*/
+  // Type system
+  var i = 0
+  def reset() { i = 0 }
+  def fresh():x = {
+    val I = i
+    i += 1
+    "%x"+I
+  }
+  def fresht():σ = tx(fresh())
 
-  % Type system
-  reset :- bb_put(i,0).
-  fresh(T) :- bb_get(i,I), format(atom(T),'%x~w',[I]), I1 is I + 1, bb_put(i,I1).
-
-  % 3.4 Kinded Unification
-
+  // 3.4 Kinded Unification
+/*
   F1 ⊆ F2 :- intersection(F2,F1,F1_),length(F1,L),length(F1_,L).
   ±(F1, F2, F) :-
     dom(F1,Dom1),dom(F2,Dom2),union(Dom1,Dom2,Dom),
@@ -250,7 +245,11 @@ object mss2 {
 
   dom(F,Dom) :- maplist([L:_,L]>>!,F,Dom).
 
-  u(K,E,(K0,S)) :- u(E,K,[],[],(_, K0, S, _)).
+  def u:(K:Map[x,k],E:E):(Map[x,k],Map[x,t]) = {
+    val (_,K0,S,_) = u(E,K,Map(),Map())
+    (K0,S)
+  }
+
   u([],K,S,SK,([],K,S,SK)) /*:- writeln(b:u([],K,S,SK))*/.
   u(E,K,S,SK,(E_,K_,S_,SK_)) :-
     u((E,K,S,SK) ⟹ (E1,K1,S1,SK1)),!,
@@ -260,9 +259,9 @@ object mss2 {
     u(E1,K1,S1,SK1,(E_,K_,S_,SK_)).
 
   %u((E,K,S,SK) ⟹ _) :- writeln(a:u(E,K,S,SK)),fail.
-  %(i) type
+  // (i) type
   u(([(τ,τ)|E],K,S,SK) ⟹ (E,K,S,SK)).
-  %(ii) 
+  // (ii) 
   u(([(t,τ)|E],K0,S,SK) ⟹ (E_,K_,S_,SK_)) :-
     t(t), ftv(τ,FTV), \+member(t,FTV),
     member(t:u,K0), subtract(K0,[t:u],K),!,
@@ -274,7 +273,7 @@ object mss2 {
     u(([(t,τ)|E],[t:u|K0],S,SK) ⟹ (E_,K_,S_,SK_)).
   u(([(∀(t,k,t1),τ)|E],K0,S,SK) ⟹ (E_,K_,S_,SK_)) :-
     u(([(t1,τ)|E],[t:k|K0],S,SK) ⟹ (E_,K_,S_,SK_)).
-  %(iii) record
+  // (iii) record
   u(([(t1,t2)|E],K0,S,SK) ⟹ (E_, K_, S_,SK_)) :-
     t(t1),t(t2),
     member(t1:F1,K0), record(l:q,F1), member((t2,F2),K0), record(l:q,F2), subtract(K0,[(t1,F1),(t2,F2)],K),
@@ -284,7 +283,7 @@ object mss2 {
     ksub([t2/t1],K,K1),'±'(F1, F2, F12),tsub([t2/t1],F12,F12_),union(K1,[(t2,F12_)],K_),
     ssub([t2/t1],S,S1), union(S1,[t2/t1],S_),
     ssub([t2/t1],SK,SK1), union(SK1,[F1/t1],SK_).
-  %(iv) record2
+  // (iv) record2
   u(([(t1,F2)|E],K0,S,SK) ⟹ (E_,K_, S_,SK_)) :- record(l:q,F2),
     member(t1:F1,K0), record(l:q,F1), subtract(K0,[t1:F1],K),
     dom(F1,Dom1), dom(F2,Dom2),Dom1 ⊆ Dom2, ftv(F2,FTV), \+member(t, FTV),
@@ -293,12 +292,12 @@ object mss2 {
     ksub([F2/t1],K,K_),
     ssub([F2/t1],S,S1), union(S1,[F2/t1],S_),
     ssub([F2/t1],SK,SK1), union(SK1,[F1/t1],SK_).
-  %(v) record3
+  // (v) record3
   u(([(F1,F2)|E],K,S,SK) ⟹ (E_,K,S,SK)) :- record(l:q,F1),record(l:q,F2),
     dom(F1,Dom1), dom(F2,Dom2), Dom1=Dom2,
     maplist({F1,F2}/[L,(Ft1,Ft2)]>>(member(L:Ft1,F1),member(L:Ft2,F2)),Dom1,ED),
     union(E,ED,E_).
-  %(vi) variant
+  // (vi) variant
   u(([(t1,t2)|E],K0,S,SK) ⟹ (E_,K_,S_,SK_)) :-
     t(t1),t(t2),
     member((t1,{F1}),K0),list(l:q,F1), member((t2,{F2}),K0),list(l:q,F2), subtract(K0,[(t1,{F1}),(t2,{F2})],K),
@@ -308,7 +307,7 @@ object mss2 {
     ksub([t2/t1],K,K1), ±(F1,F2, F12), tsub([t2/t1],{F12},{F12_}), union(K1,[(t2,{F12_})],K_),
     ssub([t2/t1],S,S1), union(S1,[t2/t1],S_),
     ssub([t2/t1],SK,SK1), union(SK1,[{F1}/t1],SK_).
-  %(vii) variant2
+  // (vii) variant2
   u(([(t1,{F2})|E],K0,S,SK) ⟹ (E_,K_,S_,SK_)) :- t(t1), list(l:q,F2),
     member((t1:{F1}),K0),list(l:q,F1), subtract(K0,[(t1:{F1})],K),
     dom(F1,Dom1), dom(F2,Dom2), Dom1 ⊆ Dom2, ftv(F2,FTV), \+member(t1, FTV),
@@ -317,16 +316,16 @@ object mss2 {
     ksub([{F2}/t1],K,K_),
     ssub([{F2}/t1],S,S1), union(S1,[{F2}/t1],S_),
     ssub([{F2}/t1],SK,SK1), union(SK1,[{F1}/t1],SK_).
-  % (viii) variant3
+  // (viii) variant3
   u(([({F1},{F2})|E],K,S,SK) ⟹ (E_,K,S,SK)) :- list(l:q,F1),list(l:q,F2),
     dom(F1,Dom), dom(F2,Dom),
     maplist({F1,F2}/[L,(Ft1,Ft2)]>>(member(L:Ft1,F1),member(L:Ft2,F2)),Dom,ED),
     union(E, ED, E_).
-  %(ix) arr
+  // (ix) arr
   u(([((τ11->τ21),(τ12->τ22))|E],K,S,SK) ⟹ (E_,K,S,SK)) :-
     union(E,[(τ11,τ12),(τ21,τ22)],E_).
 
-  % alorighm WK
+  // alorighm WK
 
   foldxq((X,∀(T_,K,Q),Ks,S),(X_,Q_,[Si:K|Ks_],[Si/T_|S_])) :-
     fresh(Si),!,foldxq(((X!Si),Q,Ks,S),(X_,Q_,Ks_,S_)).
@@ -337,60 +336,80 @@ object mss2 {
     eftv(K, τ,τFTV), eftv(K, T, TFTV), subtract(τFTV, TFTV, ts),
     findall((Ti:Ki),(member(Ti:Ki,K),member(Ti,ts)),K1),
     subtract(K,K1,K0),foldr([Ti:Ki,τi,∀(Ti,Ki,τi)]>>!,K1,τ,τ_).
-
-  wk(K,_,I,(K,[],I,int)) :- i(I).
-  wk(K,_,true,(K,[],true,bool)) :- !.
-  wk(K,_,false,(K,[],false,bool)) :- !.
-  wk(K,T,x,(K_,[],x_,Sτ2)) :-
-    x(x),
-    member(x:Sτ, T),
-    foldxq((x,Sτ,[],[]),(x_,Sτ1,SKs,S)),
-    maplist({S}/[(Si:Ki),(Si:Ki_)]>>ksub(S,Ki,Ki_),SKs,SKs_),union(K,SKs_,K_),  tsub(S,Sτ1,Sτ2).
-  wk(K,T,λ(x,E1), (K1,S1,λ(x:t_,M1),(t_->t1))) :-
-    fresh(t), wk([t:u|K],[x:t|T],E1, (K1,S1,M1,t1)), tsub(S1,t,t_).
-  wk(K,T,(E1$E2),(K3,S321,(M1_ $ M2_),t3)) :-
-    wk(K,T,E1,(K1,S1,M1,σ1)), subT(S1,T,T1),
-    wk(K1,T1,E2,(K2,S2,M2,σ2)), tsub(S2,σ1,σ1_),
-    fresh(t), u(K2,[(σ1_,(σ2->t))],(K3,S3)),
-    union(S3,S2,S32), union(S32,S1,S321),
-    mtsub(S32,M1,M1_), mtsub(S3,M2,M2_), tsub(S3,t,t3).
-  wk(K,_,[],(K,[],[],[])).
-  wk(K,T,[L1=E1|LEs],(Kn,S_,[L1=M1|LMs],[L1:τ1|LTs])) :-
-    wk(K,T,E1,(K1,S1,M1,τ1)),
-    subT(S1,T,T1),
-    wk(K1,T1,LEs,(Kn,S,LMs,LTs)),
-    union(S1,S,S_).
-  wk(K,T,(E1#L),(K2,S,((M1_:t2_) # L),t1_)) :-
-    wk(K,T,E1, (K1,S1,M1,τ1)),fresh(t1),fresh(t2),
-    u([t1:u,t2:[L:t1]|K1],[(t2,τ1)],(K2,S2)),
-    union(S2,S1,S), msub(S,M1,M1_),
-    tsub(S,t1,t1_), tsub(S,t2,t2_).
-  wk(K,T,modify(E1,L,E2),(K3,S321,modify(M1_:t2_,L,M2_),t2_)) :-
-    wk(K,T,E1,(K1,S1,M1,τ1)),subT(S1,T,T1),
-    wk(K1,T1,E2,(K2,S2,M2,τ2)),
-    tsub(S2,τ1,τ1_),fresh(t1),fresh(t2),
-    u([t1:u,t2:[L:t1]|K2],[(t1,τ2),(t2,τ1_)],(K3,S3)),
-    union(S3,S2,S32),union(S32,S1,S321),
-    msub(S32,M1,M1_),tsub(S3,t2,t2_),msub(S3,M2,M2_).
-  wk(K,T,case(E0,{Les}), (Kn1,S_, case(M0_,{LMs_}),t0_)) :-
-    wk(K,T,E0,(K0,S0,M0,τ0)), subT(S0,T,T0), fresh(t0),
-    foldr([Li=Ei,(Ki1,Ti1,LMs1,Lts1,K1,Tts1,S1),
-      (Ki,Ti,[Li=Mi|LMs1],[Li:ti|Lts1],[ti:u|K1],[(τi,ti)|Tts1],S)]>>(
-      wk(Ki1,Ti1,Ei,(Ki,Si,Mi,τi)), subT(Si,Ti1,Ti),
-      union(Si,S1,S),fresh(ti)
-    ),Les,(K0,T0,[],[],Kn,[],[]),(Kn,_,LMs,Lts,Kn_,Tts,S)),
-    maplist({t0,S}/[(τi,ti),(τi_,(t0->ti))]>>tsub(S,τi,τi_),Tts,Tts_),
-    tsub(S,τ0,τ0_), union([(τ0_,{Lts})],Tts_,Tts2),
-    u(Kn_,Tts2, (Kn1,Sn1)),union(Sn1,S,S_),
-    tsub(Sn1,t0,t0_),msub(S_,M0,M0_),
-    maplist({S}/[Li=Mi,Li=Mi_]>>msub(S,Mi,Mi_),LMs,LMs_).
-  wk(K,T,{[L=E1]},([t:{[L:τ1]}|K1],S1,({[L=M1]}:t),t)) :-
-    wk(K,T,E1,(K1,S1,M1,τ1)),fresh(t).
-  wk(K,T,(let(X=E1);E2),(K2,S21,(let(X:σ1_=poly(M1_:σ1_)); M2),τ2)) :-
-    wk(K,T,E1,(K1,S1,M1,τ1)),!,subT(S1,T,T1),
-    cls(K1,T1,τ1,(K1_,σ1)),
-    wk(K1_,[X:σ1|T1],E2,(K2,S2,M2,τ2)),!,
-    tsub(S2,σ1,σ1_),msub(S2,M1,M1_),
-    union(S2,S1,S21).
   */
+  def wk(K:Map[x,k],T:Map[x,σ],e:E):(Map[x,k],Map[x,M],M,σ) = e match {
+    case EInt(i) => (K,Map(),MInt(i),tint)
+    case ETrue => (K,Map(),MTrue,tbool)
+    case EFalse => (K,Map(),MFalse,tbool)
+    /*
+    case Ex(x) =>
+      val (x_,sτ1,SKs,S)=foldxq((x,T(x),[],[]))
+      val SKs_ = SKs.map{(Si,Ki)=>(Si,ksub(S,Ki))}
+      (K ++ SKs_,Map(),x_,tsub(S,sτ1))
+    case EAbs(x,e1) =>    
+      val t = fresht()
+      val (K1,S1,M1,t1) = wk(K+(t->U),T+(x->t),e1)
+      val t_ = tsub(S1,t)
+      (K1,S1,MAbs(x,t_,M1),tarr(t_,t1))
+    case EApp(e1,e2) =>
+      val (K1,S1,M1,σ1) = wk(K,T,E1)
+      val (K2,S2,M2,σ2) = wk(K1,subT(S1,T),E2)
+      val t = fresht()
+      val (K3,S3) = u(K2,List(tsub(S2,σ1)->tarr(σ2,t)))
+      val S32 = S3++S2
+      (K3,S32++S1,MApp(mtsub(S32,M1), mtsub(S3,M2)),tsub(S3,t))
+    */
+    case ERecord(List()) => (K,Map(),MRecord(List()),trecord(List()))
+    /*
+    case ERecord((l1,e1)::les) =>
+      val (k1,s1,m1,τ1) = wk(K,T,e1)
+      val (kn,s,MRecord(lMs),trecord(lTs)) = wk(k1,subT(s1,T),les)
+      (kn,s1++s,MRecord((l1->m1)::lMs),trecord((l1->τ1)::lTs))
+    case EDot(e1,l) =>
+      val (K1,S1,M1,τ1) = wk(K,T,e1)
+      val t1 = fresht()
+      val t2 = fresht()
+      val (K2,S2) = u([t1:u,t2:[l:t1]|K1],[(t2,τ1)])
+      val S = S2++S1
+      (K2,S,MDot(msub(S,M1),tsub(S,t2), l),tsub(S,t1))
+    case EModify(e1,l,e2) =>
+      val (K1,S1,M1,τ1) = wk(K,T,e1)      
+      val (K2,S2,M2,τ2) = wk(K1,subT(S1,T),e2)
+      val τ1_ = tsub(S2,τ1)
+      val t1 = fresht()
+      val t2 = fresh()
+      val (K3,S3) = u(K2++Map(t1->U,t2->krecord(List(l->t1))),List((t1,τ2),(t2,τ1_)))
+      val S32 = S3++S2
+      val S321 = S32++S1
+      val t2_ = tsub(S3,t2)
+      (K3,S321,MModify(msub(S32,M1),t2_,l,msub(S3,M2)),t2_)
+    case ECase(E0,les) =>
+      val (K0,S0,M0,τ0) = wk(K,T,E0)
+      val t0 = fresh()
+      foldr([Li=Ei,(Ki1,Ti1,LMs1,Lts1,K1,Tts1,S1),
+        (Ki,Ti,[Li=Mi|LMs1],[Li:ti|Lts1],[ti:u|K1],[(τi,ti)|Tts1],S)]>>(
+        wk(Ki1,Ti1,Ei,(Ki,Si,Mi,τi)), subT(Si,Ti1,Ti),
+        union(Si,S1,S),fresh(ti)
+      ),les,(K0,subT(S0,T),[],[],Kn,[],[]),(Kn,_,LMs,Lts,Kn_,Tts,S)),
+      maplist({t0,S}/[(τi,ti),(τi_,(t0->ti))]>>tsub(S,τi,τi_),Tts,Tts_),
+      tsub(S,τ0,τ0_), union([(τ0_,{Lts})],Tts_,Tts2),
+      u(Kn_,Tts2, (Kn1,Sn1)),union(Sn1,S,S_),
+      tsub(Sn1,t0,t0_),msub(S_,M0,M0_),
+      maplist({S}/[Li=Mi,Li=Mi_]>>msub(S,Mi,Mi_),LMs,LMs_)
+      (Kn1,S_, MCase(M0_,LMs_),t0_)
+    */
+    case EVariant(l,e1) =>
+      val (k1,s1,m1,τ1) = wk(K,T,e1)
+      val t = fresh()
+      (k1+(t->kvariant(List(l->τ1))),s1,MVariant(l,m1,tx(t)),tx(t))
+    /*
+    case ELet(x,e1,e2) =>
+      val (K1,S1,m1,τ1) = wk(K,T,e1)
+      val T1 = subT(S1,T)
+      val (K1_,σ1) = cls(K1,T1,τ1)
+      val (K2,S2,M2,τ2) = wk(K1_,T1+(x->σ1),e2)
+      val σ1_ = tsub(S2,σ1)
+      (K2,S2++S1,MLet(x,σ1_,MPoly(msub(S2,m1),σ1_),M2),τ2)
+    */
+  }
 }
